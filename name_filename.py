13197def97788e4d239213e6d4caf903916e13b5
AlokM02 <90358671+AlokM02@users.noreply.github.com>
2023-03-13 12:10:04 +1000

    update to the script

--- name_filename.py
+++ name_filename.py
@@ -1,19 +1,19 @@
 import os
 import pandas as pd
 
-# Load the dataset into a pandas dataframe
-df = pd.read_excel('your_dataset_file_name.xlsx')
+# read in the mapping of file names to shortened file names
+df = pd.read_excel('file_rename.xlsx')
 
-# Iterate through all the files in the current directory and its subdirectories
-for root, dirs, files in os.walk("."):
+# loop through all files in current directory and its subdirectories
+for root, dirs, files in os.walk('.'):
     for filename in files:
-        # Check if the file name contains any of the strings from the 'File Name' column in the dataset, ignoring case
-        for file_str in df['File Name']:
-            if file_str.lower() in filename.lower():
-                # Replace the matched part of the file name with the corresponding string from the 'Shortened File Name' column
-                new_filename = filename.lower().replace(file_str.lower(), df[df['File Name'].str.lower()==file_str.lower()]['Shortened File Name'].values[0])
-                # Restore the original case of the filename
-                new_filename = filename[:filename.lower().find(file_str.lower())] + new_filename + filename[filename.lower().find(file_str.lower())+len(file_str):]
-                # Rename the file
+        # check if the filename contains any substring from the 'File Name' column
+        for file_name in df['File Name']:
+            if file_name.lower() in filename.lower():
+                # replace the matching substring with the 'Shortened File Name' value
+                shortened_name = df.loc[df['File Name'] == file_name, 'Shortened File Name'].values[0]
+                new_filename = filename.lower().replace(file_name.lower(), shortened_name)
+                # rename the file with the new filename
                 os.rename(os.path.join(root, filename), os.path.join(root, new_filename))
-                print(f"Renamed {filename} to {new_filename}")
+                print(f"Renamed {os.path.join(root, filename)} to {new_filename}")
+                break
